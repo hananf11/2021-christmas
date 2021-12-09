020d6e85a049a16414d32ca3026b04acfa4451c0
Hanan F <hananfokkens@gmail.com>
2021-12-10 11:20:10 +1300

    cf186c8

---
+++ _nuxt/static/1639088252/10/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/10", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1639088252/8/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F2021-christmas\u002F_nuxt\u002Fstatic\u002F1639088252",layout:"default",error:a,serverRendered:true,routePath:"\u002F8",config:{_app:{basePath:"\u002F2021-christmas\u002F",assetsPath:"\u002F2021-christmas\u002F_nuxt\u002F",cdnURL:a}}}}(null));
